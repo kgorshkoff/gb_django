6426073de4d0b99350a9c8a847029c8fcce4fd9c
gobzter@gmail.com <123321Qq!>
2018-11-29 10:31:44 +0300

    hw_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+import datetime
+import json
+import os
 
 
-# Create your views here.
 def main_view(request):
-    return render(request, 'index.html')
+    title = 'Главная'
+    date = datetime.date.today().year
+    content = {'date': date, 'title': title}
+    return render(request, 'index.html', content)
 
 
 def catalog(request):
-    return render(request, 'catalog.html')
+    title = 'Каталог'
+    date = datetime.date.today().year
+
+    path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'static', 'data.json'))
+    with open(path) as json_data:
+        products = json.load(json_data)
+        json_data.close()
+
+    content = {'title': title, 'products': products, 'date': date}
+    return render(request, 'catalog.html', content)
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = 'Контакты'
+    date = datetime.date.today().year
+    content = {'title': title, 'date': date}
+    return render(request, 'contacts.html', content)
