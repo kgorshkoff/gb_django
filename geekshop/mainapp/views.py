6d29d6ae768714ff528f2be0f0f7e35c388495bd
gobzter@gmail.com <123321Qq!>
2018-11-29 11:20:08 +0300

    hw_2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -6,26 +6,23 @@ import os
 
 def main_view(request):
     title = 'Главная'
-    date = datetime.date.today().year
-    content = {'date': date, 'title': title}
+    content = {'title': title}
     return render(request, 'index.html', content)
 
 
 def catalog(request):
     title = 'Каталог'
-    date = datetime.date.today().year
 
     path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'static', 'data.json'))
     with open(path) as json_data:
         products = json.load(json_data)
         json_data.close()
 
-    content = {'title': title, 'products': products, 'date': date}
+    content = {'title': title, 'products': products}
     return render(request, 'catalog.html', content)
 
 
 def contacts(request):
     title = 'Контакты'
-    date = datetime.date.today().year
-    content = {'title': title, 'date': date}
+    content = {'title': title}
     return render(request, 'contacts.html', content)
